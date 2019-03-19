2624a9f9c486c4ad8f37f041154ed6bc97bb529f
ling-pjh <48480857+ling-pjh@users.noreply.github.com>
2019-03-19 23:12:27 +0800

    修改了签到中界面，添加了头像显示

---
+++ app/src/main/java/com/example/meetingpad/Adapter/AttendMeetingListAdapter.java
@@ -0,0 +1,109 @@
+package com.example.meetingpad.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.meetingpad.R;
+import com.example.meetingpad.entity.PersonLight;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttendMeetingListAdapter extends RecyclerView.Adapter {
+
+    private int count=0;
+    private List<PersonLight> persons;
+    private Context context;
+    public AttendMeetingListAdapter(Context context, List<PersonLight> persons){
+
+        this.persons=persons;
+        this.context=context;
+    }
+
+    class HeadImageViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView headImageView;
+        private TextView nameTextView;
+
+        public HeadImageViewHolder(final View itemView) {
+            super(itemView);
+            nameTextView=itemView.findViewById(R.id.name_in_head_image);
+            headImageView=itemView.findViewById(R.id.image_in_head_image);
+        }
+
+        public ImageView getHeadImageView() {
+            return headImageView;
+        }
+
+        public TextView getNameTextView() {
+            return nameTextView;
+        }
+
+
+    }
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        HeadImageViewHolder hivh;
+            hivh=new HeadImageViewHolder(((Activity)context).getLayoutInflater().inflate(R.layout.head_image,null));
+        return hivh;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        HeadImageViewHolder vh = ((HeadImageViewHolder) holder);
+            String name;
+            byte[] headImage;
+            Bitmap bitmap = null;
+            name = persons.get(position).getpName();
+            headImage = persons.get(position)
+                    .getpIcon();
+            if (headImage != null)
+                bitmap = BitmapFactory.decodeByteArray(headImage, 0, headImage.length);
+            if (name == null) name = "空";
+            if (bitmap == null) {
+            headImage = getByte(context, R.drawable.ic_account_circle_black_30dp);
+            bitmap=BitmapFactory.decodeByteArray(headImage,0,headImage.length);
+            }
+            vh.getNameTextView().setText(name);
+            vh.getHeadImageView().setImageBitmap(bitmap);
+        }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+
+    public void setPersons(ArrayList<PersonLight> persons) {
+        this.persons = persons;
+    }
+
+    public  byte[] getByte(Context context, int vectorDrawableId) {
+        Bitmap bitmap = null;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            Drawable vectorDrawable = context.getDrawable(vectorDrawableId);
+            bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                    vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            vectorDrawable.draw(canvas);
+        } else {
+            bitmap = BitmapFactory.decodeResource(context.getResources(), vectorDrawableId);
+        }
+        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,baos);
+        byte[] datas=baos.toByteArray();
+        return datas;
+    }
+
+}
--- app/src/main/java/com/example/meetingpad/CheckingActivity.java
+++ app/src/main/java/com/example/meetingpad/CheckingActivity.java
@@ -1,13 +1,28 @@
 package com.example.meetingpad;
 
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import com.example.meetingpad.Adapter.AttendMeetingListAdapter;
+import com.example.meetingpad.entity.PersonLight;
+
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckingActivity extends AppCompatActivity {
 
     private TextView tvMeetingRoomId;
+    private RecyclerView checkedPeopleImages,notCheckPeopleImages;
+    private AttendMeetingListAdapter checkedPeopleAdapter,notCheckPeopleAdapter;
+    private List<PersonLight> checkedPeople,notCheckPeople;
+    private TextView infomationTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -15,8 +30,46 @@ public class CheckingActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("签到中");
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            int i;
             tvMeetingRoomId=(TextView)findViewById(R.id.tv_meeting_room_id_checking);
             tvMeetingRoomId.setText(getIntent().getStringExtra("meetingRoomId"));
+
+            checkedPeople=new ArrayList<>();
+            for(i=1;i<=30;i++)checkedPeople.add(new PersonLight());
+            notCheckPeople=new ArrayList<>();
+            for(i=1;i<=30;i++)notCheckPeople.add(new PersonLight());
+
+
+            checkedPeopleAdapter=new AttendMeetingListAdapter(this,checkedPeople);
+            notCheckPeopleAdapter=new AttendMeetingListAdapter(this,notCheckPeople);
+
+
+            checkedPeopleImages=(RecyclerView)findViewById(R.id.recycler_view_checked_in_checking);
+            checkedPeopleImages.setLayoutManager(new GridLayoutManager(this,6));
+            notCheckPeopleImages=(RecyclerView)findViewById(R.id.recycler_view_not_check_in_checking);
+            notCheckPeopleImages.setLayoutManager(new GridLayoutManager(this,6));
+
+            checkedPeopleImages.addItemDecoration(new RecyclerView.ItemDecoration() {
+                @Override
+                public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                    super.getItemOffsets(outRect, view, parent, state);
+                    outRect.right=20;
+                    outRect.bottom=20;
+                }
+            });
+
+            notCheckPeopleImages.addItemDecoration(new RecyclerView.ItemDecoration() {
+                @Override
+                public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                    super.getItemOffsets(outRect, view, parent, state);
+                    outRect.right=20;
+                    outRect.bottom=20;
+                }
+            });
+
+            checkedPeopleImages.setAdapter(checkedPeopleAdapter);
+            notCheckPeopleImages.setAdapter(notCheckPeopleAdapter);
+
     }
 
     @Override
