ae4f3c959d17aecdb4294821cabf88f02417c9b2
ling-pjh <48480857+ling-pjh@users.noreply.github.com>
2019-03-19 22:37:43 +0800

    修改了空闲界面

--- app/src/main/java/com/example/meetingpad/FreeActivity.java
+++ app/src/main/java/com/example/meetingpad/FreeActivity.java
@@ -1,15 +1,21 @@
 package com.example.meetingpad;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class FreeActivity extends AppCompatActivity {
 
+    private ImageView qrCodeView;
+    private TextView titleTV,timeTV,informationTV;
+    private View goMoreMeetingView;
     private TextView tvMeetingRoomId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,14 @@ public class FreeActivity extends AppCompatActivity {
         tvMeetingRoomId=(TextView)findViewById(R.id.tv_meeting_room_id_free);
         Intent i=getIntent();
         tvMeetingRoomId.setText(i.getStringExtra("meetingRoomId"));
+
+
+        qrCodeView=(ImageView)findViewById(R.id.qr_code_image_view);
+        titleTV=(TextView)findViewById(R.id.tv_meeting_title_in_free);
+        timeTV=(TextView)findViewById(R.id.tv_meeting_time_in_free);
+        informationTV=(TextView)findViewById(R.id.tv_meeting_information_in_free);
+        goMoreMeetingView=(View)findViewById(R.id.go_more_meeting_view);
+
 
     }
 
